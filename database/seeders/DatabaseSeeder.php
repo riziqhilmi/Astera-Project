80399acc12858ceeeca02a464a12b54d9d8c6fb4
khfadam <biruhijauproject@gmail.com>
2025-08-08 09:16:09 +0700

    Data Menu  User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Create default user for login
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@astera.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'User Test',
+            'email' => 'user@astera.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
     }
 }
